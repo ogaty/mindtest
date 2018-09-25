9107fe1869a84c18e80ce8f052ba44fb57936aee
ogaty <trytodream1221@gmail.com>
2018-09-26 07:41:26 +0900

    first

---
+++ Droid/Fragments/IFragmentVisible.cs
@@ -0,0 +1,7 @@
+﻿namespace kuruapp.Droid
+{
+    interface IFragmentVisible
+    {
+        void BecameVisible();
+    }
+}
---
+++ Droid/Fragments/TrainingFragment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Android.OS;
+using Android.Views;
+
+namespace kuruapp.Droid
+{
+    public class TrainingFragment : Android.Support.V4.App.Fragment, IFragmentVisible
+    {
+        public static TrainingFragment NewInstance() =>
+            new TrainingFragment { Arguments = new Bundle() };
+   
+        public TrainingFragment()
+        {
+        }
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View view = inflater.Inflate(Resource.Layout.fragment_browse, container, false);
+            return view;
+        }
+
+        public void BecameVisible()
+        {
+        }
+    }
+}
